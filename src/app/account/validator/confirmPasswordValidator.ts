69b1ce777df1f51626df8ff58ce90db13d2c2fa0
peimingjun <pei_mingjun@wuxiapptec.com>
2019-12-07 20:33:20 +0800

    http-utils

--- src/app/account/validator/confirmPasswordValidator.ts
+++ src/app/account/validator/confirmPasswordValidator.ts
@@ -1,7 +1,7 @@
 import { ValidatorFn, AbstractControl } from '@angular/forms';
 export function confirmPasswordValidator(sourceControl: AbstractControl): ValidatorFn {
-    return (control: AbstractControl): {[key: string]: any} => {
-      const result = control.value != sourceControl.value;
-      return result ? {'confirmPassword': {value: control.value}} : null;
-    };
-  }
+  return (control: AbstractControl): { [key: string]: any } => {
+    const result = control.value != sourceControl.value;
+    return result ? { 'confirmPassword': { value: control.value } } : null;
+  };
+}
